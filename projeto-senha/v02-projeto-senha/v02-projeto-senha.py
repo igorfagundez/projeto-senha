0d4c21c4e617d6ab33b3f331f99c0cd067ee3289
igorfagundez <igorfagundesfio@gmail.com>
2023-09-12 00:32:41 -0300

    projeto v02 finalizado

--- projeto-senha/v02-projeto-senha/v02-projeto-senha.py
+++ projeto-senha/v02-projeto-senha/v02-projeto-senha.py
@@ -10,7 +10,7 @@ class PassGen:
             [sg.Text('Site/Software', size=(10, 1)),
              sg.Input(key='site', size=(20, 1))],
             [sg.Text('E-mail/Usuario', size=(10, 1)), 
-            sg.input(key='usuario', size=(20, 1))],
+            sg.Input(key='usuario', size=(20, 1))],
             [sg.Text('Quantidade de caracteres'), sg.Combo(values=list(
                 range(30)), key='total_chars', default_value=1, size=(3, 1))],
             [sg.Output(size=(32, 5))],
@@ -24,9 +24,21 @@ class PassGen:
             evento, valores = self.janela.read()
             if evento == sg.WINDOW_CLOSED:
                 break
+            if evento == 'Gerar Senha':
+                nova_senha = self.gerar_senha(valores)
+                self.salvar_senha(nova_senha, valores)
+    def gerar_senha(self, valores):
+        char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&*'
+        chars = random.choices(char_list, k=int(valores['total_chars']))
+        new_pass = ''.join(chars)
+        return new_pass
 
-    def salvar_senha(self):
-        pass
+    
+    def salvar_senha(self, nova_senha, valores):
+        with open('senhas.txt','a', newline='') as arquivo:
+            arquivo.write(f"site: {valores['site']}, usuario: {valores['usuario']}, nova senha: {nova_senha}")
+        
+        print(f"O arquivo foi salvo e a senha é: \n\n{nova_senha}")
 
 gen = PassGen()
-gen.Iniciar()
+gen.Iniciar() 
